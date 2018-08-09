d34b187b9f8f48f34ec6a8a5233afc128ec16746
Moroine Bentefrit <moroine.bentefrit@adactive.com>
2018-08-09 18:42:26 +0800

    WIP:
    need to figure out https://github.com/awslabs/aws-sam-cli/issues/105

--- src/resources/list.js
+++ src/resources/list.js
@@ -1,9 +1,22 @@
+import { DynamoDB } from 'aws-sdk';
+
+const docClient = new DynamoDB.DocumentClient();
+const params = {
+  TableName: process.env.TABLE_NAME,
+};
+
 const listResource = (event, context, callback) => {
-  callback(null, {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'List resource',
-    }),
+
+  docClient.get(params, (err, data) => {
+    if (err) {
+      console.log('Error', err);
+      callback(null, { statusCode: 500, body: 'OUps' });
+    } else {
+      callback(null, {
+        statusCode: 200,
+        body: 'Fuck it', //  JSON.stringify(data),
+      });
+    }
   });
 };
 
--- src/resources/post.js
+++ src/resources/post.js
@@ -1,9 +1,39 @@
+import { DynamoDB } from 'aws-sdk';
+
+const docClient = new DynamoDB.DocumentClient();
+const params = {
+  TableName: process.env.TABLE_NAME,
+};
+
 const postResource = (event, context, callback) => {
-  callback(null, {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Post resource',
-    }),
+  let body = null;
+  try {
+    body = JSON.parse(event.body);
+  } catch (e) {
+    callback(null, {
+      statusCode: 400,
+      body: {
+        success: false,
+        error: 'POST body is not a valid JSON object',
+      },
+    });
+
+    return;
+  }
+
+  params.Item = {
+    id: body.resourceId,
+  };
+
+  docClient.put(params, (err, data) => {
+    if (err) {
+      callback(err);
+    } else {
+      callback(null, {
+        statusCode: 200,
+        body: JSON.stringify(data),
+      });
+    }
   });
 };
 
